49cca2979f6a852637c2857f826f4b3ad64d3aed
wamozo cosmas <wcosmas75@gmail.com>
2020-09-03 14:20:27 +0300

    changes

--- controllers/sms.js
+++ controllers/sms.js
@@ -3,21 +3,6 @@ require("dotenv").config();
 const apiKey = process.env.YOUR_API_KEY;
 const username = process.env.YOUR_USERNAME;
 
-<<<<<<< HEAD
-const apiKey = process.env.YOUR_API_KEY;
-const username = process.env.YOUR_USERNAME;
-
-const sendSms = (phone, message) => {
-  const Africastalking = require('africastalking')(apiKey, username);
-  Africastalking.SMS //client.messages
-    .create({
-       body: message,
-       from: process.env.PHONE_NUMBER,
-       to: phone
-     })
-    .then(message => console.log(message.sid)).catch(err=>{
-        console.log(err);
-=======
 
 const sendSms = (phone, message) => {
   const AfricasTalking = require("africastalking")({apiKey,username});
@@ -34,7 +19,6 @@ const sendSms = (phone, message) => {
     })
     .catch((error) => {
       console.log(error);
->>>>>>> 2b4f57615991aab8f041d5db490ac51282de6ba6
     });
 };
 
