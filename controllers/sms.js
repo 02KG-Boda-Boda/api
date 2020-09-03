9ebcf6bae77ce92914b655b87ff39b11356536da
aaron <akizzam@2gofinancialservices.com>
2020-09-03 11:05:31 +0000

    changes from server

--- controllers/sms.js
+++ controllers/sms.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const apiKey = process.env.YOUR_API_KEY;
+const username = process.env.YOUR_USERNAME;
 
 const sendSms = (phone, message) => {
-  const client = require('twilio')(accountSid, authToken);
-  client.messages
+  const Africastalking = require('africastalking')(apiKey, username);
+  Africastalking.SMS //client.messages
     .create({
        body: message,
-       from: process.env.TWILIO_PHONE_NUMBER,
+       from: process.env.PHONE_NUMBER,
        to: phone
      })
     .then(message => console.log(message.sid)).catch(err=>{
